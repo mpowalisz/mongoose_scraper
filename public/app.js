892cf729d0d0e93813aac01d4e4c57379010d9e4
mpowalisz <markpowalisz@gmail.com>
2018-09-21 12:04:24 -0400

    Upgraded CSS. Made links clickable

--- public/app.js
+++ public/app.js
@@ -3,13 +3,23 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append(
+      "<div class='article' data-id=" +
+        data[i]._id +
+        ">" +
+        data[i].title +
+        "</div>" +
+        "<a href=" +
+        data[i].link +
+        '">' +
+        data[i].link +
+        "/><br><br>"
+    );
   }
 });
 
-
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", ".article", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
@@ -30,7 +40,9 @@ $(document).on("click", "p", function() {
       // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append(
+        "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
+      );
 
       // If there's a note in the article
       if (data.note) {
